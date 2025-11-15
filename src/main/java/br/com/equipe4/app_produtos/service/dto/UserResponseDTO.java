3688559727891b414c960933bc3f0dda50fceac8
Aoife <imatt.lino@gmail.com>
2025-11-15 14:42:13 -0300

    tentando novamente adicionar DTOs, Token JWT, reps

---
+++ src/main/java/br/com/equipe4/app_produtos/service/dto/UserResponseDTO.java
@@ -0,0 +1,7 @@
+package br.com.equipe4.app_produtos.service.dto;
+
+import br.com.equipe4.app_produtos.model.UserRole;
+
+public record UserResponseDTO(String id, String login, UserRole role) {
+
+}
